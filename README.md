9b73d1beecbe04ad98807549cf218eb5bbd04e2e
zlsheepcity <zl.sheepcity@gmail.com>
2026-01-27 14:40:54 +0200

    Project routes structure

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # testapp-nextjs-forms-and-tables
-Basic example for nextjs app
+Basic example for nextjs app  
+v2026.1.27  
+
+# Workflow snippets
+```bash
+npx create-next-app@latest my-app --yes
+```
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,7 +3,7 @@ export default function Page() {
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-16 px-16 bg-white dark:bg-black sm:items-start">
 
-        <h1>Home</h1>
+        <h1>Login</h1>
 
         <nav>
           <ul>
